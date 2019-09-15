6c024d8d680cf48745ad1975b69c95941e609185
Justin Lam <justinmklam@gmail.com>
2019-09-15 12:30:24 -0700

    Add another print message to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "\033[0;32mRebuilding website...\033[0m\n"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
@@ -23,6 +23,8 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
+
+printf "\033[0;32mPushing updates to GitHub...\033[0m\n"
 
 # Push source and build repos.
 git push origin master
